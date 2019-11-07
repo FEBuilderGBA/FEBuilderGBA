c6691bf57f1511c1511dfd867f4b478aef9df0b7
7743 <jmp.ebx@gmail.com>
2019-11-07 22:00:35 +0900

    ver_20191107.22

--- FEBuilderGBA/MapSettingDifficultyForm.Designer.cs
+++ FEBuilderGBA/MapSettingDifficultyForm.Designer.cs
@@ -33,9 +33,9 @@
             this.OKButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.customColorGroupBox1 = new FEBuilderGBA.CustomColorGroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.J_HardBoost = new System.Windows.Forms.Label();
+            this.J_NormalPenalty = new System.Windows.Forms.Label();
+            this.J_EasyPenalty = new System.Windows.Forms.Label();
             this.EasyPenalty = new System.Windows.Forms.NumericUpDown();
             this.HardBoost = new System.Windows.Forms.NumericUpDown();
             this.NormalPenalty = new System.Windows.Forms.NumericUpDown();
@@ -96,9 +96,9 @@
             // customColorGroupBox1
             // 
             this.customColorGroupBox1.BorderColor = System.Drawing.Color.Empty;
-            this.customColorGroupBox1.Controls.Add(this.label1);
-            this.customColorGroupBox1.Controls.Add(this.label2);
-            this.customColorGroupBox1.Controls.Add(this.label3);
+            this.customColorGroupBox1.Controls.Add(this.J_HardBoost);
+            this.customColorGroupBox1.Controls.Add(this.J_NormalPenalty);
+            this.customColorGroupBox1.Controls.Add(this.J_EasyPenalty);
             this.customColorGroupBox1.Controls.Add(this.EasyPenalty);
             this.customColorGroupBox1.Controls.Add(this.HardBoost);
             this.customColorGroupBox1.Controls.Add(this.NormalPenalty);
@@ -109,41 +109,41 @@
             this.customColorGroupBox1.TabStop = false;
             this.customColorGroupBox1.Text = "難易度補正";
             // 
-            // label1
+            // J_HardBoost
             // 
-            this.label1.AccessibleDescription = "@MAPSETTING_HARD_BOOST";
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(7, 27);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(311, 32);
-            this.label1.TabIndex = 59;
-            this.label1.Text = "ハードブースト";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.J_HardBoost.AccessibleDescription = "@MAPSETTING_HARD_BOOST";
+            this.J_HardBoost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.J_HardBoost.Location = new System.Drawing.Point(7, 27);
+            this.J_HardBoost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.J_HardBoost.Name = "J_HardBoost";
+            this.J_HardBoost.Size = new System.Drawing.Size(311, 32);
+            this.J_HardBoost.TabIndex = 59;
+            this.J_HardBoost.Text = "ハードブースト";
+            this.J_HardBoost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // J_NormalPenalty
             // 
-            this.label2.AccessibleDescription = "@MAPSETTING_NORMAL_MODE_LEVEL_PENALTY";
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(7, 84);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(311, 32);
-            this.label2.TabIndex = 60;
-            this.label2.Text = "ノーマルモードペナルティ";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.J_NormalPenalty.AccessibleDescription = "@MAPSETTING_NORMAL_MODE_LEVEL_PENALTY";
+            this.J_NormalPenalty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.J_NormalPenalty.Location = new System.Drawing.Point(7, 84);
+            this.J_NormalPenalty.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.J_NormalPenalty.Name = "J_NormalPenalty";
+            this.J_NormalPenalty.Size = new System.Drawing.Size(311, 32);
+            this.J_NormalPenalty.TabIndex = 60;
+            this.J_NormalPenalty.Text = "ノーマルモードペナルティ";
+            this.J_NormalPenalty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
+            // J_EasyPenalty
             // 
-            this.label3.AccessibleDescription = "@MAPSETTING_EAZY_MODE_LEVEL_PENALTY";
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(7, 115);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(311, 32);
-            this.label3.TabIndex = 61;
-            this.label3.Text = "簡易モードペナルティ";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.J_EasyPenalty.AccessibleDescription = "@MAPSETTING_EAZY_MODE_LEVEL_PENALTY";
+            this.J_EasyPenalty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.J_EasyPenalty.Location = new System.Drawing.Point(7, 115);
+            this.J_EasyPenalty.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.J_EasyPenalty.Name = "J_EasyPenalty";
+            this.J_EasyPenalty.Size = new System.Drawing.Size(311, 32);
+            this.J_EasyPenalty.TabIndex = 61;
+            this.J_EasyPenalty.Text = "簡易モードペナルティ";
+            this.J_EasyPenalty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // EasyPenalty
             // 
@@ -215,9 +215,9 @@
 
         private System.Windows.Forms.NumericUpDown DifficultyValue;
         private System.Windows.Forms.Label panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label J_HardBoost;
+        private System.Windows.Forms.Label J_NormalPenalty;
+        private System.Windows.Forms.Label J_EasyPenalty;
         private System.Windows.Forms.NumericUpDown HardBoost;
         private System.Windows.Forms.NumericUpDown NormalPenalty;
         private System.Windows.Forms.NumericUpDown EasyPenalty;
