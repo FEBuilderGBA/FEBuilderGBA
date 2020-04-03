898bd9a82db23b4bf92ea61e7e54a6e413229ab4
7743 <jmp.ebx@gmail.com>
2020-04-03 20:09:24 +0900

    ver_20200403.20

--- FEBuilderGBA/bin/Debug/config/patch2/FE8U/kagaribi3/kagaribi3.s
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8U/kagaribi3/kagaribi3.s
@@ -1,7 +1,6 @@
 @霧の中でも特定の座標に視界を確保します。
 @
-@Call 08019CD6 (FE8J) <-
-@Call 0801A164 (FE8U)
+@Call 08019FFE (FE8U)<-
 @
 @関数末尾をフックするので全部使えます.
 @r0 temp
@@ -24,7 +23,8 @@
 @
 @
 .thumb
-	ldr  r4, =0x0202bcec @ @ChapterData
+@	ldr  r4, =0x0202bcec @ @ChapterData	@{J}
+	ldr  r4, =0x0202BCF0 @ @ChapterData	@{U}
 	ldr  r6,KAGARIBI_Table
 
 	sub r6,r6,#0x08
@@ -44,8 +44,8 @@ Loop:
 	cmp  r0,#0x00
 	beq  AddPoint
 
-	blh  0x080860D0   @CheckFlag {J}
-@	blh  0x08083DA8   @CheckFlag {U}
+@	blh  0x080860D0   @CheckFlag {J}
+	blh  0x08083DA8   @CheckFlag {U}
 	cmp  r0,#0x00
 	beq  Loop
 
@@ -54,22 +54,24 @@ AddPoint:
 	ldrb r1, [r6, #0x2]
 	ldrb r2, [r6, #0x3]
 	mov r3, #0x1
-	blh 0x0801a798   @MapAddInRange
+@	blh 0x0801a798   @MapAddInRange {J}
+	blh 0x0801aabc   @MapAddInRange {U}
 	b   Loop
 
 Term:
 
 @壊すコードの再送
-ldr r0, =0x0202BCEC @(ChapterData@ステージの領域.Clock )
+@ldr  r0, =0x0202bcec @ @ChapterData	@{J}
+ldr  r0, =0x0202BCF0 @ @ChapterData	@{U}
 ldrb r0, [r0, #0xf] @(ChapterData@ステージの領域.フェイズ 0=自軍,0x40=友軍,0x80=敵軍 )
 cmp r0, #0x80
 beq SkipEnd
 	mov r7, #0x81
-	ldr r1, =0x08019CE0+1
+	ldr r1, =0x0801A008+1
 	b Join
 
 SkipEnd:
-	ldr r1, =0x8019da8+1
+	ldr r1, =0x801a0d0+1
 
 Join:
 bx  r1
