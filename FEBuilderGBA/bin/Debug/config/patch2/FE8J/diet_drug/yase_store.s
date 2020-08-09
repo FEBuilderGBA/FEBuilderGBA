3bf1bf8f1c7defe2fe6c81223c42ba8c4a464011
7743 <jmp.ebx@gmail.com>
2020-08-09 12:02:40 +0900

    ver_20200809.12

--- FEBuilderGBA/bin/Debug/config/patch2/FE8J/diet_drug/yase_store.s
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8J/diet_drug/yase_store.s
@@ -2,13 +2,16 @@
 @0x2F7B0 からか、ここに飛んで来るコードをかかないとダメです。
 @
 	ldrb r1, [r4, #0x1a]
-	add r0 ,r0, r1
-	cmp     r0,#0xf		@マイナスに突っ込んだ場合
+	add  r0 ,r0, r1
+	strb r0, [r4, #0x1a]
+
+	ldrb r0, [r4, #0x1a]
+	cmp     r0,#0x80		@マイナスに突っ込んだ場合
 	blt     Skip
 	mov     r0,#0x0     @マイナスにはできないので0に補正する
+	strb r0, [r4, #0x1a]
 
 Skip:
-	strb r0, [r4, #0x1a]
 
 	mov r0 ,r4
 
