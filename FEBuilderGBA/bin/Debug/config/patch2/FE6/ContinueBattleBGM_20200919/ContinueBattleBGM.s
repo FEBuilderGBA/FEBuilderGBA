c4c8fe577ebf34b649a7eade322caf5ba3c1ea08
7743 <jmp.ebx@gmail.com>
2020-09-19 21:30:10 +0900

    ver_20200919.21

---
+++ FEBuilderGBA/bin/Debug/config/patch2/FE6/ContinueBattleBGM_20200919/ContinueBattleBGM.s
@@ -0,0 +1,37 @@
+@
+@Call  0805BD78    FE6
+@
+@r0 Nazo
+@
+@
+@r4 Change SongID
+@
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+.thumb
+
+ldr   r3, =0x02023CBC   @ FE6 (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+
+ldrh  r3, [r3, #0x4]   @      (BGMSTRUCT@BGM.再生しているBGM )
+cmp   r3, r4
+beq   Exit
+	@@mov r0 ,r2      @不要
+	blh 0x08003028    @FE6 SomethingSoundRelated_80022EC
+
+	mov r0 ,r4
+	blh 0x080033c8    @FE6
+	b Exit
+
+SomeBGM:
+	ldr   r3, =0x02023CBC   @ FE6 (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+	@待機BGMを0にする
+	mov   r0, #0x0
+	strh  r0, [r3, #0x2]
+Exit:
+pop {r4}
+pop {r0}
+bx r0
---
+++ FEBuilderGBA/bin/Debug/config/patch2/FE7U/ContinueBattleBGM_20200919/ContinueBattleBGM.s
@@ -0,0 +1,43 @@
+@
+@@Call  08067E8C    FE7J
+@Call 080676A0    FE7U
+@
+@r0 Nazo
+@
+@
+@r4 Change SongID
+@
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+.thumb
+
+@ldr   r3, =0x02024E14   @ FE7J (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+ldr   r3, =0x02024E1C  @ FE7U (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+
+ldrh  r3, [r3, #0x4]   @      (BGMSTRUCT@BGM.再生しているBGM )
+cmp   r3, r4
+beq   SomeBGM
+	@@mov r0 ,r2      @不要
+	@blh 0x0800348c    @FE7J SomethingSoundRelated_80022EC
+	blh 0x080035b0   @FE7U SomethingSoundRelated_80022EC
+
+	mov r0 ,r4
+	@blh 0x0800394c    @FE7J
+	blh 0x08003a70   @FE7U
+	b Exit
+
+SomeBGM:
+	@ldr   r3, =0x02024E14   @ FE7J (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+	ldr   r3, =0x02024E1C  @ FE7U (BGMSTRUCT@BGM.音楽関係のフラグ1 )
+	@待機BGMを0にする
+	mov   r0, #0x0
+	strh  r0, [r3, #0x2]
+
+Exit:
+pop {r4}
+pop {r0}
+bx r0
