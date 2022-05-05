fca2ebeaac3c15d3e27adf232b3d1d4490fc44bb
7743 <jmp.ebx@gmail.com>
2022-05-05 17:16:15 +0900

    ver_20220505.17

---
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8J/MultiSupplyUnits20220419/ForPrepareListMenuItem.s
@@ -0,0 +1,25 @@
+.thumb
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+@Hook	A1240	{J}
+@Hook	9EFA0	{U}
+
+blh 0x08031568 @HasConvoyAccess	{J}
+@blh 0x0803161C @HasConvoyAccess	{U}
+mov r2, #0x2
+orr r2, r0
+
+ldr r0, [r7, #0x2c]
+mov r1, r8
+ldrb r1, [r1, #0x0]
+
+blh 0x0809A4BC   @SomethingPrepListRelated	{J}
+@blh 0x080981e4   @SomethingPrepListRelated	{U}
+
+ldr r3, =0x080A124A|1	@{J}
+@ldr r3, =0x0809EFAA|1	@{U}
+bx  r3
---
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8U/MultiSupplyUnits20220419/ForPrepareListMenuItem2.s
@@ -0,0 +1,24 @@
+.thumb
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+@Hook	A13FC	{J}
+@Hook	9F15C	{U}
+
+@blh 0x08031568 @HasConvoyAccess	{J}
+blh 0x0803161C @HasConvoyAccess	{U}
+mov r2, #0x2
+orr r2, r0
+
+add r5, #0x33
+ldrb r1, [r5, #0x0]
+
+@blh 0x0809A4BC   @SomethingPrepListRelated	{J}
+blh 0x080981e4   @SomethingPrepListRelated	{U}
+
+@ldr r3, =0x080A1406|1	@{J}
+ldr r3, =0x0809F166|1	@{U}
+bx  r3
