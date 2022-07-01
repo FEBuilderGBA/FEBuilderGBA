4be59c5366535d83ca28e1c8212e5d48c4f9b93c
7743 <jmp.ebx@gmail.com>
2022-07-02 07:49:32 +0900

    ver_20220702.07

--- FEBuilderGBA/bin/Debug/config/patch2/FE8U/RNGRandomizer/ResumeHackFE8.asm
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8U/RNGRandomizer/ResumeHackFE8.asm
@@ -1,34 +1,29 @@
 .thumb
 
-pop {r3}
-
 @overwritten code
 ldrh r1,[r0]
-strh r1,[r2]
-add r0,#0x2
-ldrh r1,[r0]
 strh r1,[r2,#0x2]
-ldrh r1,[r0,#0x2]
-strh r1,[r2,#0x4]
+ldrh r0,[r2,#0x2]
+strh r0,[r2,#0x4]
 
 push {r0}
-push {r1}
 
-ldr r1,AttackerData
+ldr r3,AttackerData
 ldr r0,=#0x00000000
-str r0,[r1]
+str r0,[r3]
 
-ldr r1,ActiveUnitDeploymentNumber
+ldr r3,ActiveUnitDeploymentNumber
 ldrb r0,=#0x00
-strb r0,[r1]
+strb r0,[r3]
 
-pop {r1}
 pop {r0}
 
-@ldr r1,ReturnAddress
-bx r14
+ldr r3,ReturnAddress
+bx r3
 
 .align 4
+ReturnAddress:
+.long 0x08000C44|1
 AttackerData:
 .long 0x0203A4EC
 ActiveUnitDeploymentNumber:
