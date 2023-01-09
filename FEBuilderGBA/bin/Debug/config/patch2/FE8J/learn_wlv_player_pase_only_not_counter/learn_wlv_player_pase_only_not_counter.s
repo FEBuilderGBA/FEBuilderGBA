d1f874246c8c0a7b1cced71f31940e66f0904971
7743 <jmp.ebx@gmail.com>
2023-01-09 21:29:27 +0900

    ver_20230109.21

--- FEBuilderGBA/bin/Debug/config/patch2/FE8J/learn_wlv_player_pase_only_not_counter/learn_wlv_player_pase_only_not_counter.s
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8J/learn_wlv_player_pase_only_not_counter/learn_wlv_player_pase_only_not_counter.s
@@ -1,6 +1,13 @@
 .thumb
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
 
-@Hook 0802C090	@{J}
+@Hook 0802C080	@{J}
+
+push {r0}
 
 ldr r3, LimitWeaponLevel
 cmp r6, r3
@@ -9,20 +16,21 @@ blt AddWeaponLevel
 ldr r3, =0x0202BCEC	@ChapterInfo @{J}
 ldrb r3, [r3, #0xF] @phase
 cmp r3, #0x00
-bne Exit
+bne FalseExit
 
 AddWeaponLevel:
-add r6 ,r6, r0
+pop {r0}
+blh 0x08017540   @GetItemWExp	@{J}
+mov r1 ,r7
+add r1, #0x7b
 
-Exit:
-ldr r3, =0x0802C098|1	@{J}
-mov lr, r3
+ldr r3, =0x0802C088|1	@{J}
+bx  r3
 
-mov r1, #0x0
-ldrb r3, [r5, #0x0]
-ldr r2, [r7, #0x4]
-
-bx  lr
+FalseExit:
+pop {r0}
+ldr r3, =0x0802C092|1	@{J}
+bx  r3
 
 .ltorg
 LimitWeaponLevel:
--- FEBuilderGBA/bin/Debug/config/patch2/FE8U/learn_wlv_player_pase_only_not_counter/learn_wlv_player_pase_only_not_counter.s
+++ FEBuilderGBA/bin/Debug/config/patch2/FE8U/learn_wlv_player_pase_only_not_counter/learn_wlv_player_pase_only_not_counter.s
@@ -1,7 +1,14 @@
 .thumb
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
 
-@Hook 0802C090	@{J}
-@Hook 0802C148	@{U}
+@Hook 0802C080	@{J}
+@Hook 0802C138	@{U}
+
+push {r0}
 
 ldr r3, LimitWeaponLevel
 cmp r6, r3
@@ -11,21 +18,24 @@ blt AddWeaponLevel
 ldr r3, =0x0202BCF0	@ChapterInfo @{U}
 ldrb r3, [r3, #0xF] @phase
 cmp r3, #0x00
-bne Exit
+bne FalseExit
 
 AddWeaponLevel:
-add r6 ,r6, r0
+pop {r0}
+@blh 0x08017540   @GetItemWExp	@{J}
+blh 0x08017798   @GetItemWExp	@{U}
+mov r1 ,r7
+add r1, #0x7b
 
-Exit:
-@ldr r3, =0x0802C098|1	@{J}
-ldr r3, =0x0802C150|1	@{U}
-mov lr, r3
+@ldr r3, =0x0802C088|1	@{J}
+ldr r3, =0x0802C140|1	@{U}
+bx  r3
 
-mov r1, #0x0
-ldrb r3, [r5, #0x0]
-ldr r2, [r7, #0x4]
-
-bx  lr
+FalseExit:
+pop {r0}
+@ldr r3, =0x0802C092|1	@{J}
+ldr r3, =0x0802C14A|1	@{J}
+bx  r3
 
 .ltorg
 LimitWeaponLevel:
