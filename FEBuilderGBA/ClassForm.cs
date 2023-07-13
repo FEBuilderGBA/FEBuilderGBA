4f15f98a9232a89cb909aece689fb0b8649018df
Scott Melanson <40323442+TheGameMecha@users.noreply.github.com>
2023-07-13 17:14:55 -0400

    Removed unnecessary Using statements

--- FEBuilderGBA/ClassForm.cs
+++ FEBuilderGBA/ClassForm.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace FEBuilderGBA
 {
--- FEBuilderGBA/UnitForm.cs
+++ FEBuilderGBA/UnitForm.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.IO;
-using Microsoft.VisualBasic.FileIO;
 
 namespace FEBuilderGBA
 {
